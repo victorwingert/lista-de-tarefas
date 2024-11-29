f242f59ecee03564142104f0d03caa7e88a0564e
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-11-29 12:23:43 -0300

    change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/lista-de-tarefas',
+  base: '/gerenciador-de-tarefas',
   plugins: [react()],
   resolve: {
     alias: {
